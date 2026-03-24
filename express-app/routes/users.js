179087556669e68a2cbac513b1c881d58b928e13
Lychee <2006tka@gmail.com>
2026-03-24 18:59:27 +0500

    5-6

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const users = [];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send({'items': [{'id': 1, 'name': 'Katya'}, {'id': 2, 'name': 'Vanya'}]});
+  res.send(users);
 });
+
+router.post('/', function(req, res, next) {
+    let user = req.body;
+    users.push(user);
+    res.status(201).json(user);
+})
+
+router.get('/:id', function(req, res, next) {
+    let id = req.params.id;
+    if (users.length > id) {
+        res.send(users[id]);
+    }
+    else {
+        res.status(404).send('Not Found');
+    }
+})
 
 module.exports = router;
