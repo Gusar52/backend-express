8fe8d622fa1361cd76bee2013da2354065561bc9
Lychee <2006tka@gmail.com>
2026-03-24 19:32:42 +0500

    8 try

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,26 +1,39 @@
 const express = require('express');
 const router = express.Router();
-const users = [{'name': 'vasya'}];
+
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('mydb.db');
+db.run(`CREATE TABLE IF NOT EXISTS users (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name text)`);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(users);
+    db.all("SELECT id, name FROM users", [], (err, rows) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(rows);
+        }
+    });
 });
 
 router.post('/', function(req, res, next) {
     let user = req.body;
-    users.push(user);
+    const insert = "INSERT INTO users (user) VALUES (?)";
+    db.run(insert, [user]);
     res.status(201).json(user);
 })
 
 router.get('/:id', function(req, res, next) {
-    let id = req.params.id - 1;
-    if (users.length > id) {
-        res.send(users[id]);
-    }
-    else {
-        res.status(404).send('Not Found');
-    }
+    let id = req.params.id;
+    db.get("SELECT name FROM users WHERE id = @id", [id], (err, rows) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(rows);
+        }
+    });
 })
 
 module.exports = router;
